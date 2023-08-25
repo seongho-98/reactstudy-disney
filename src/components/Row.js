20e2be75d22afc06e25088ae749a69b96512be9d
seongho <tjdghgk98@gmail.com>
2023-08-25 17:44:17 +0900

    feat : 영화 이미지 클릭 => 모달로 상세 설명

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import myAxios from '../api/axios'
 import './Row.css'
+import MovieModal from './MovieModal';
 
 const Row = ({title, id, fetchUrl}) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelection] = useState({});
 
   // 컴포넌트가 렌더링 될 때마다, fetchMovieData가 새로 생성됨
   // 함수를 메모이제이션 해주면 성능 개선 가능
@@ -15,8 +18,11 @@ const Row = ({title, id, fetchUrl}) => {
   useEffect(() => {
     fetchMovieData();
   }, [fetchMovieData])
-  // [] : dependency 함수에 fetchUrl을 넣어주자
-  // fetchUrl이 변경되면, dependency 함수를 실행시킴
+  
+  const handleClick = (movie) => {    
+    setModalOpen(true);
+    setMovieSelection(movie)
+  }
   
   return (
     <div>
@@ -37,6 +43,7 @@ const Row = ({title, id, fetchUrl}) => {
               className='row__poster'
               src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
               alt={movie.title}
+              onClick={() => handleClick(movie)}
             ></img>
           ))}
         </div>
@@ -51,6 +58,12 @@ const Row = ({title, id, fetchUrl}) => {
           </span>
         </div>
       </div>
+      {modalOpen && 
+        <MovieModal 
+          {...movieSelected}
+          setModalOpen={setModalOpen}
+        />
+      }
     </div>
   )
 }
