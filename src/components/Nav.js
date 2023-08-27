831607c9bca647f355bd94cf480de11115cc406f
seongho <tjdghgk98@gmail.com>
2023-08-27 16:31:36 +0900

    feat : 검색 기능 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,34 @@
 import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components'
 
 function Nav() {
 
   const [show, setShow] = useState(false);
-
+  const {pathname} = useLocation(); //pathname을 가져오는 부분 
+  const [searchValue, setSearchValue] = useState('');
+  const navigate = useNavigate();
+  
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 50) {
-        setShow(true);
-      } else {
-        setShow(false);
-      }
-    });
+    window.addEventListener('scroll', handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', () => {});
+      window.removeEventListener('scroll', handleScroll);
     };
   }, [])
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  }
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value); //searchvalue의 state를 바꿔줌
+    navigate(`/search?q=${e.target.value}`);
+  }
 
   return (
     
@@ -30,11 +41,46 @@ function Nav() {
           onClick={() => window.location.href = '/'}
         />
       </Logo>
+
+      {pathname === '/' ? 
+        (<Login>Login</Login>) : 
+        <Input 
+          value={searchValue}
+          onChange={handleChange}
+          className='nav__input' 
+          type="text" 
+          placeholder='검색해주세요.'/>}
     </NavWrapper>
   )
 }
 
 export default Nav
+
+const Login = styled.a`
+  background-color: rgba(0,0,0,0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
+
+  &:hover{
+    background-color: #f9f9f9;
+    color: gray;
+    border-color: transparent;
+  }
+`;
+
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0,0,0,0.582);
+  border-radius: 5px;
+  color:white;
+  padding: 5px;
+  border: none;
+`;
 
 const NavWrapper = styled.nav`
   position:fixed;
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import Myaxios from '../../api/axios';
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
 
 const SearchPage = () => {
+  
+  const [searchResults, setSearchResults] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if(searchTerm){ //searchTerm이 있을 때
+      fetchSearchMovie(searchTerm);
+    }
+  },[searchTerm])
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try{
+      const response = await Myaxios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+      setSearchResults(response.data.results);
+      console.log(response.data.results);
+    }catch(error){
+      console.log(error);
+    }
+  }
+
   return (
     <div> SearchPage</div>
   )
