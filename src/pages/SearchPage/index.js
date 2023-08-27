0fb7f028f33830eeb1823df0898ea9465d79bfc0
seongho <tjdghgk98@gmail.com>
2023-08-27 17:15:03 +0900

    feat : 검색 부분 UI 작성

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,10 +1,12 @@
 import Myaxios from '../../api/axios';
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import "./SearchPage.css"
 
 const SearchPage = () => {
   
   const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
 
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);
@@ -29,9 +31,43 @@ const SearchPage = () => {
     }
   }
 
-  return (
-    <div> SearchPage</div>
+  if(searchResults.length > 0){
+  return ( 
+    <section className='search-container'>
+      {searchResults.map((movie) => {
+        if(movie.backdrop_path !== null && movie.media_type !== "person"){
+          const movieImageUrl = 
+            "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+          
+            return(
+              <div>
+                <div className="movie" key={movie.id}>
+                  <div
+                    onClick={() => navigate(`/${movie.id}`)}
+                    className='movie__column-poster'
+                  >
+                    <img src={movieImageUrl}
+                      alt="movie"
+                      className='movie__poster'
+                    />
+                  </div>
+                </div>
+              </div>
+            )
+        }
+      })}
+
+    </section>
   )
+  }else{
+    return (
+      <div className='no-results'>
+        <p className='no-results__text'>
+          찾고자 하는 검색어 "{searchTerm}"이 없습니다.
+        </p>
+      </div>
+    )
+  }
 }
 
 export default SearchPage
