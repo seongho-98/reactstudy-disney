fa0009e50b5b53c1b2d80816aa473a2c58d665fa
seongho <tjdghgk98@gmail.com>
2023-08-27 17:42:25 +0900

    feat : 이미지 클릭 시 이미지 크게 보이기

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import MyAxios from '../../api/axios'
 
 const DetailPage = () => {
-  return (
-    <div> DetailPage</div>
+  let {movieId} = useParams();
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    //함수 만들고
+    async function fetchData(){
+      const request = await MyAxios.get(`https://api.themoviedb.org/3/movie/${movieId}`);
+      setMovie(request.data);
+      console.log(request);
+    }
+
+    fetchData()
+  }, [movieId]);
+
+  if(!movie){
+    return null;
+  }
+
+  return(
+    <section>
+      <img
+        className='modal__poster-img'
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="modal__poster-img"
+      />
+    </section>
   )
+  
 }
 
 export default DetailPage
